fd30a6ce1c126811ccf4f5365d16cc4b9cc5e4a1
parmis mohaghegh <parmis.mohaghegh@alumni.ubc.ca>
2018-09-28 13:08:32 -0700

    added tests

--- src/main/java/mp1/Document.java
+++ src/main/java/mp1/Document.java
@@ -97,14 +97,31 @@ public class Document implements Comparable<Document> {
         // a simpler constructor that sets the id to be the url
         this(url, url);
     }
+    /**
+     * Return the number of words in the document
+     *
+     *
+     * @return the total number of words in the document
+     */
 
     public int getTotalWords(){
         return this.totalWords;
     }
-
+    /**
+     * Return the sentiment score of the document.
+     *
+     *
+     * @return the sentiment score of the document
+     */
     public int getCalculatedSent(){
         return sentimentScore;
     }
+    /**
+     * Return the map of words in the document and their number of occurrences
+     *
+     *
+     * @return the word-map of the document
+     */
     public HashMap<String,Integer> getWordMap(){
         return this.wordMap;
     }
@@ -131,6 +148,7 @@ public class Document implements Comparable<Document> {
             double prob_mean = (prob1+ prob2)/2.0;
             double firstTerm=0;
             double secondTerm=0;
+
             if (prob1!=0){
                 firstTerm = prob1*logbase2(prob1/prob_mean);
             }
@@ -142,7 +160,15 @@ public class Document implements Comparable<Document> {
         double JSD = sum/2;
         return Math.round(JSD*100);
     }
+    /**
+     * Compute the probability of occurrence of a word in the document.
+     *
+     * @param myMap is not null
+     * @param key the word to calculate the probability of
+     * @param size total number of words contained in the document
+     * @return the probability of occurrence the word "key" in the document (this)
 
+     */
 
     public double calcProb(HashMap<String, Integer> myMap, String key, int size){
         if(!myMap.containsKey(key)){
@@ -150,6 +176,13 @@ public class Document implements Comparable<Document> {
         }
         return (double) myMap.get(key)/ size;
     }
+
+    /**
+     * Compute the logarithm with base 2 of a given number.
+     *
+     * @param num is not null and nonzero
+     * @return the logarithm with base 2 of a given number
+     */
 
     public double logbase2(double num){
         return Math.log(num)/Math.log(2);
--- src/test/java/mp1/MP1TestPublic2.java
+++ src/test/java/mp1/MP1TestPublic2.java
@@ -137,5 +137,10 @@ public class MP1TestPublic2 {
         assertEquals(expPair, docPair);
     }
 
+//    @Test
+//    public void contains_test(){
+//        Document doc=null;
+//        assertTrue(false,contains(doc);
+//    }
 
     }
